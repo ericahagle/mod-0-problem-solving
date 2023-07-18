d15acb39fde717f15f74e3aba523821d42ffbb94
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-18 11:16:44 -0700

    Complete challenge 1

--- challenge1.js
+++ challenge1.js
@@ -1,2 +1,22 @@
 // Given an array of strings, print only the strings that have exactly 4 characters.
 
+// GOAL: Print strings with exactly 4 characters out of an array of strings
+
+// Create an array of strings
+var strArray = ["this", "array", "has", "more", "than", "four", "strings"];
+
+// Create a function
+    // Loop through the strings
+        // Determine all strings.length === 4
+            // Print all strings.length === 4
+
+function strictlyFour(array) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i].length === 4) {
+            console.log(array[i]);
+        }
+    }
+};
+
+// Call the function
+strictlyFour(strArray);
