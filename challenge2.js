b4446dd274d4f912566f4b0ba5be0756d944313f
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-18 11:26:47 -0700

    Complete challenge 2

--- challenge2.js
+++ challenge2.js
@@ -1,2 +1,21 @@
 // Start with an array of strings with a mix of uppercase and lowercase letters. Print every word in all lowercase letters.
 
+// GOAL: Convert all strings in an array to lowercase and print them to the console.
+
+// Create array of strings with a mix of uppercase and lowercase letters
+var strArray = ["STRING", "gOlF", "dICe", "i wOUlD LIkE To rAGE"];
+
+// Create a function
+    // Loop through the strings in the array
+        // Convert strings to all lowercase letters
+            // Print all strings
+
+function convertToLower(array) {
+    for (var i = 0; i < array.length; i++) {
+        array[i] = array[i].toLowerCase();
+        console.log(array[i]);
+    }
+};
+
+// Call the function
+convertToLower(strArray);
