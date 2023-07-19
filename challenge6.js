f2f8f5f0a84c8363a4ebdb25d4690a1be3a8f765
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-19 11:05:06 -0700

    Complete challenge 6

--- challenge6.js
+++ challenge6.js
@@ -1,2 +1,22 @@
 // Given a sentence with only lowercase letters, print the same sentence with the first letter of every word capitalized. For example, if you were given "Turing is the best", return "Turing Is The Best" instead!
 
+// GOAL: Convert an all lowercase sentence to Title Case
+
+// Create a function that accepts a string as an argument
+    // Split string into an array of strings (words)
+    // Loop through the array
+        // Convert first character of each word to Upper Case
+        // Add the remaining characters of each word to the first char
+    // Rejoin the array to make a string (sentence)
+// Print the Title Case sentence to the console
+
+function lowerToTitle(str) {
+    str = str.toLowerCase().split(" ");
+    for (var i = 0; i < str.length; i++) {
+        str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
+    }
+    return str.join(" ");
+};
+
+// Call the function
+console.log(lowerToTitle("this sentence should be in title case."));
