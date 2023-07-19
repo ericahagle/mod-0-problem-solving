0c02216037cb01d761c37b69383855a19402bb84
Erica Hagle <ericahagle.dev@gmail.com>
2023-07-19 11:08:45 -0700

    Add comment to 'challenge7.js'

--- challenge7.js
+++ challenge7.js
@@ -1,2 +1,5 @@
 // Write a method or function that determines how much a person will pay in taxes in the United States (https://www.irs.gov/newsroom/irs-provides-tax-inflation-adjustments-for-tax-year-2022) based on their annual income. The method or function should accept a number representing the individual's annual income as an argument and return the amount they will owe in taxes for that year.
 
+
+
+/* Might try to complete this (and submit again) if I have time, or after evals (for fun!) but I'm going to submit to AirTable now to make sure I get everything else in on time! */
